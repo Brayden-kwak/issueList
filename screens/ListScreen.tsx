80aea939949fe6ef85b5c7808906f238cc9657fa
Jihoon <playingcards@naver.com>
2022-11-28 16:10:23 +0900

    before work detail screen

--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -1,14 +1,25 @@
-import {View, Text, FlatList, TouchableOpacity, Dimensions, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import IssueApi, {IssueList} from '../api/IssueApi';
 import {Holder} from '../components/Holder';
 import {FootHolder} from '../components/FootHolder';
 import styled from 'styled-components/native';
 import SortComments from '../components/SortComments';
+import {RootStackParams} from '../App';
 
 const Width = Dimensions.get('window').width - 30;
 
-const ListScreen = () => {
+type Props = NativeStackScreenProps<RootStackParams, 'ListDetailScreen'>;
+
+const ListScreen = ({navigation}: Props) => {
   const [issueLists, setIssueLists] = useState<IssueList[] | null>(null);
 
   useEffect(() => {
@@ -19,50 +30,48 @@ const ListScreen = () => {
     })();
   }, []);
 
-  const IssueRenderList = (issue: IssueList) => {
-    const createdDate =
-      issue.item.created_at.slice(0, 4) +
-      '년' +
-      issue.item.created_at.slice(5, 7) +
-      '월' +
-      issue.item.created_at.slice(8, 9) +
-      '일';
-    return (
-      <View style={{width: Width}}>
-        <TouchableOpacity style={{flexDirection: 'row'}} onPress={() => navigation}>
-          <View style={{flexDirection: 'column'}}>
-            <SubTitle numberOfLines={1}>
-              #{issue.item.number} {issue.item.title}
-            </SubTitle>
-            <SubTitle2 numberOfLines={1}>
-              <Text>
-                작성자:{issue.item.user.login} | 작성일: {createdDate}
-              </Text>
-            </SubTitle2>
-          </View>
-          <SubTitle3>코멘트: {issue.item.comments}</SubTitle3>
-        </TouchableOpacity>
-      </View>
-    );
-  };
-
   return (
     <SafeAreaView>
-        <Container>
+      <Container>
         <Title>ListScreen</Title>
         <FlatList
-            data={issueLists}
-            scrollEnabled={true}
-            keyExtractor={(item, index) => index.toString()}
-            showsVerticalScrollIndicator={false}
-            ListFooterComponent={() => <FootHolder px={50} />}
-            ItemSeparatorComponent={() => <Holder px={Width} />}
-            renderItem={({item, index}) => {
-            return <IssueRenderList key={index} item={item} />;
-            }}
-            onEndReached={() => {}}
+          data={issueLists}
+          scrollEnabled={true}
+          keyExtractor={(item, index) => index.toString()}
+          showsVerticalScrollIndicator={false}
+          ListFooterComponent={() => <FootHolder px={50} />}
+          ItemSeparatorComponent={() => <Holder px={Width} />}
+          renderItem={({item, index}) => {
+            const createdDate =
+              item.created_at.slice(0, 4) +
+              '년' +
+              item.created_at.slice(5, 7) +
+              '월' +
+              item.created_at.slice(8, 9) +
+              '일';
+            return (
+              <View style={{width: Width}}>
+                <TouchableOpacity
+                  style={{flexDirection: 'row'}}
+                  onPress={() => navigation.navigate('ListDetailScreen')}>
+                  <View style={{flexDirection: 'column'}}>
+                    <SubTitle numberOfLines={1}>
+                      #{item.number} {item.title}
+                    </SubTitle>
+                    <SubTitle2 numberOfLines={1}>
+                      <Text>
+                        작성자:{item.user.login} | 작성일: {createdDate}
+                      </Text>
+                    </SubTitle2>
+                  </View>
+                  <SubTitle3>코멘트: {item.comments}</SubTitle3>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+          onEndReached={() => {}}
         />
-        </Container>
+      </Container>
     </SafeAreaView>
   );
 };
